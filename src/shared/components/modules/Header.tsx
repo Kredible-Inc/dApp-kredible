493fc2c4362279b24ed23cf96790086944564264
villarley <santivillarley1010@gmail.com>
2025-07-13 19:45:07 -0600

    refactor: update Header component to use image and link for branding
    - Replace static title with an image and link for the dApp Kredible branding
    - Adjust header layout for improved visual presentation

--- src/shared/components/modules/Header.tsx
+++ src/shared/components/modules/Header.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import ConnectWallet from "@/shared/components/ConnectWallet";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="border-b border-border p-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <h1 className="text-xl font-semibold text-foreground">
-            dApp Kredible
-          </h1>
+          <div className="flex justify-center items-center">
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src="/kredible-white.jpg"
+                alt="dApp Kredible"
+                width={75}
+                height={50}
+              />
+              <h1 className="text-2xl font-semibold text-foreground">
+                Kredible
+              </h1>
+            </Link>
+          </div>
         </div>
         <div className="flex items-center gap-4">
           <ConnectWallet />
